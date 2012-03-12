04b2a370f259cef931a4c58867c70b55397b52c9
nafur <nafur@cipher-code.de>
2012-03-12 11:05:32 +0100

    css and icons

---
+++ static/std.css.py
@@ -0,0 +1,46 @@
+<%
+req.content_type = "text/css"
+%>
+a {
+	text-decoration: underline;
+	color: black;
+}
+.box {
+	position: relative;
+	border: 1px solid silver;
+	padding: 5px;
+	vertical-align: top;
+}
+
+img.icon {
+	width: 32px;
+	height: 32px;
+}
+img.mini {
+	width: 16px;
+	height: 16px;
+}
+.volume {
+	padding: 5px;
+	margin: -12px;
+	float: right;
+	text-align: center;
+	width: 40px;
+}
+.current {
+	margin-right: 62px;
+	border: 0px;
+}
+.controls {
+	margin-top: 5px;
+	margin-right: 62px;
+	border: 0px;
+}
+.syscontrols {
+	float: right;
+	text-align: right;
+}
+.bottombox {
+	position: absolute;
+	bottom: 0px;
+}
